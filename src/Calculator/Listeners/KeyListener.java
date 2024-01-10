6c4159546695ce04a7698eeb84ce56b4d04aa559
Niklas <Max_Niklas@outlook.de>
2024-01-10 13:27:51 +0100

    inline comments added

--- src/Calculator/Listeners/KeyListener.java
+++ src/Calculator/Listeners/KeyListener.java
@@ -1,29 +1,36 @@
 package Calculator.Listeners;
 
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import static Calculator.Handlers.CallHandler.callHandler;
 
 public class KeyListener implements java.awt.event.KeyListener {
 
+    // We don't need the keyTyped or keyReleased Method, so we simply let them empty.
+    // The implementation is still needed though we have to fulfill the implement contract with KeyListener
     @Override
     public void keyTyped(KeyEvent e) {}
 
+    // we add some easy translations for multiplication and division. I used the Symbols which are not found on the keyboard
+    // simply for appearance choice.
+    // then the callHandler Calls is called which will then route the input to the correct Handler
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyChar() == '*') {
             callHandler("×");
         }
-        if (e.getKeyChar() == '/') {
+        else if (e.getKeyChar() == '/') {
             callHandler("÷");
         }
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+        else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             callHandler("=");
         }
-        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE)
+        else if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
             callHandler("⌫");
-        callHandler(Character.toString(e.getKeyChar()));
+        }
+        else {
+            callHandler(Character.toString(e.getKeyChar()));
+        }
     }
 
     @Override
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -2,7 +2,11 @@ package Calculator;
 
 import Calculator.Windows.CalculatorFrame;
 
+
+// Main class with main function -> just calls CalculatorFrame and set it to visible
+
 public class Main {
+
     public static void main(String[] args){
 
         CalculatorFrame cf = new CalculatorFrame();
